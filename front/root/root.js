1da9b3dcae40ff480877f4dd93c2c8084d609351
Davi-SO <80834681+Davi-SO@users.noreply.github.com>
2021-11-29 21:23:18 -0300

    Update root.js

--- front/root/root.js
+++ front/root/root.js
@@ -1,11 +1,15 @@
-class Menu{
+class Screen{
     constructor(){
 
         this.options = 2; 
-        this.container = this.assemble_menu();
-        console.log(this.container)
+        this.menu = this.assemble_menu();
+        console.log(this.menu)
         this.classlist = "Menu";
+        this.content = document.createElement('div')
+        this.content.id = 'content-box';
+        document.body.appendChild(this.content);
         this.open_eventos();
+
 
     }
     assemble_menu(){        
@@ -23,6 +27,9 @@ class Menu{
         home.innerText = 'Home';
         eventos.id = 'eventos';
         eventos.innerText = 'Eventos';
+        eventos.onclick = ()=>{
+            document.getElementById('eventosDiv').style.left = '0%'
+        }
         simulação.id = 'simulação';
         simulação.innerText = 'Simulação';
         simulação.href = '../entrada/entrada.html';
@@ -40,13 +47,47 @@ class Menu{
     }
     open_eventos(){
         const eventosDiv = document.createElement('div');
-        eventosDiv.id = 'eventosDiv';
-        document.body.appendChild(eventosDiv);
-
-        const calendar = document.createElement('div');
-        calendar.classList = 'calendar';
-        eventosDiv.appendChild(calendar);
         
+        eventosDiv.id = 'eventosDiv';
+        this.content.appendChild(eventosDiv);
+
+        const calendar = document.createElement('table');
+        const m = document.createElement('thead');
+        const w = document.createElement('tr');
+        w.style.height = '35px';
+        w.style.width = '700px';
+        const d = document.createElement('tr');
+        d.style.height = 'xx';
+        d.style.width = '100%';
+
+        m.innerText = 'DEZEMBRO'
+        calendar.classList = 'calendar';
+
+        eventosDiv.appendChild(calendar);
+        calendar.appendChild(m);
+        calendar.appendChild(w);
+        calendar.appendChild(d);
+        let days = ['D','S','T','Q','Q','S','S']
+
+        for(let i=0;i<7;i++){
+            const wd = document.createElement('td');
+            w.appendChild(wd);
+            wd.innerText = days[i];
+            wd.style.border = 'transparent'
+            wd.style.height = '35px';
+            wd.style.width = '35px';
+        }
+
+        for(let i=0;i<35;i++){
+            const dc = document.createElement('td');
+            d.appendChild(dc);
+            dc.id = `december${i-2}`;
+            dc.style.boxShadow = '1px 1px 5px rgb(1, 1, 1)'
+            if(i<3||i>33) dc.style.visibility = 'hidden';
+            else dc.innerText = i-2
+        }
+
+
     }
 }
-const x = new Menu;
+const x = new Screen;
